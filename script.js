abbe6c87efa55452f690c0c88afe48bfc51001e8
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-21 15:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
-    //Write your code here
+  // Get the form element
+  const form = document.getElementById("form1");
 
+  // Get values of First Name and Last Name using their name attributes
+  const firstName = form.elements["fname"].value;
+  const lastName = form.elements["lname"].value;
+
+  // Create the output message
+  const fullName = `${firstName} ${lastName}`;
+
+  // Display the output in an alert
+  alert(fullName);
+
+  // Prevent the default form submission behavior
+  return false;
 }
